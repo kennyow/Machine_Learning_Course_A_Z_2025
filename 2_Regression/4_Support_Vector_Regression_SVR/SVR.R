e1da6023e340d7e01d9a2255cd4721674bb69425
kennyow <kennyow86@gmail.com>
2025-06-12 12:01:50 -0300

    Add SVR R

---
+++ 2_Regression/4_Support_Vector_Regression_SVR/SVR.R
@@ -0,0 +1,56 @@
+#SVR
+# got from regression template
+
+#Regression Template
+
+# Data processing
+
+#Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3] 
+
+
+#Splitting the dataset into the Training set and Test set
+
+
+#install.packages('caTools')
+# library(caTools)
+# 
+# set.seed(123) #python is for test set, here is for training set
+# split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+# 
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+#Future Scaling (Euclidian distance)
+
+# training_set[, 2:3] = scale(training_set[, 2:3]) #all lines and columns 2 and 3
+# test_set[, 2:3] = scale(test_set[, 2:3])
+
+
+# Fitting the SVR to the dataset
+#install.packages('e1071')
+library(e1071)
+regressor = svm(formula = Salary ~ Level,
+                data = dataset,
+                type = 'eps-regression')
+
+
+#Predicting a new result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+
+#Visualising the SVR results (for higher resolution and smooother curve)
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             color = 'red') + 
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            color = 'blue') +
+  ggtitle("Truth or Bluff (SVR)") +
+  xlab('Level')+
+  ylab('Salary')
+
+
+
